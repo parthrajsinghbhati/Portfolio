9209510fb9e9b5c0f378dc8122e8b12d9a1eecab
Parthraj Singh Bhati <parthrajsinghbhati@gmail.com>
2026-05-18 16:00:01 +0530

    feat: add interactive SplashCursor fluid simulation background

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 import Services from "../components/Services";
 import Work from "../components/Work";
+import SplashCursor from "../components/SplashCursor";
 
 export default function Home() {
 
@@ -21,17 +22,29 @@ export default function Home() {
  },[])
 
  useEffect(()=>{
-    if(isDarkMode){
-      document.documentElement.classList.add('dark');
-      localStorage.theme = 'dark';
-    }else{
-      document.documentElement.classList.remove('dark');
-      localStorage.theme = '';
-    }
- },[isDarkMode])
+     if(isDarkMode){
+       document.documentElement.classList.add('dark');
+       localStorage.theme = 'dark';
+     }else{
+       document.documentElement.classList.remove('dark');
+       localStorage.theme = '';
+     }
+  },[isDarkMode])
 
   return (
     <>
+    <SplashCursor
+      SIM_RESOLUTION={128}
+      DYE_RESOLUTION={1440}
+      DENSITY_DISSIPATION={3.5}
+      VELOCITY_DISSIPATION={2}
+      PRESSURE={0.1}
+      CURL={3}
+      SPLAT_RADIUS={0.2}
+      SPLAT_FORCE={6000}
+      COLOR_UPDATE_SPEED={10}
+      zIndex={50}
+    />
     <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode}/>
     <Header isDarkMode={isDarkMode} />
     <About isDarkMode={isDarkMode} />
